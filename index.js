16b70e6e748ff2eb76db2d9e48886dae51c21e67
Roberto Flores <me@robertos.page>
2020-03-21 12:52:37 -0600

    verify execSync

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
-
+const execSync = require('child_process').execSync;
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
+    execSync(`echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list`, {stdio: 'inherit'});
+    execSync(`sudo apt-get install apt-transport-https ca-certificates gnupg`, {stdio: 'inherit'});
+    execSync(`curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -`, {stdio: 'inherit'});
+    /*
     const sa = core.getInput('service_account');
     const pn = core.getInput('project_name');
     core.debug('Service Account');
@@ -19,6 +23,7 @@ async function run() {
     console.log(`S2 - log`);
     core.endGroup();
     core.setOutput('time', 'test time is' + new Date().toTimeString());
+    */
   } 
   catch (error) {
     core.setFailed(error.message);
