b1ff9efc841e7415ba3510bf94b23c8fb46197eb
Roberto Flores <me@robertos.page>
2020-03-24 18:50:35 -0600

    Reduce the flow

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const core = require('@actions/core');
 const execSync = require('child_process').execSync;
 const fs = require('fs');
@@ -7,21 +9,7 @@ async function run() {
   try {
     const serviceAccountFile = `/tmp/${(new Date()).getTime()}.json`;
     const projectName = core.getInput('project_name');
-    /*
-    core.startGroup('Add Cloud SDK distribution URI as a package source');
-    execSync(`echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list`, {stdio: 'inherit'});
-    core.endGroup();
-    core.startGroup('Install apt-transport-https ca-certificates gnupg');
-    execSync(`sudo apt-get install apt-transport-https ca-certificates gnupg`, {stdio: 'inherit'});
-    core.endGroup();
-    core.startGroup('Import Google Cloud public key');
-    execSync(`curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -`, {stdio: 'inherit'});
-    core.endGroup();
-    core.startGroup('Update and install Google Cloud SDK');
-    execSync(`sudo apt-get update && sudo apt-get install google-cloud-sdk -y`, {stdio: 'inherit'});
-    core.endGroup();
-    */
-    core.startGroup('Working with SERVICE_ACCOUNT');
+    core.startGroup('Processing SERVICE_ACCOUNT');
     console.log('Copy SERVICE_ACCOUNT');
     fs.writeFileSync(serviceAccountFile, core.getInput('service_account'));
     console.log('Activate SERVICE_ACCOUNT');
@@ -30,17 +18,17 @@ async function run() {
     core.startGroup('Set GCP Project');
     execSync(`gcloud config set project ${projectName}`, {stdio: 'inherit'});
     core.endGroup();
-    if(core.getInput('gae_info')) {
-      core.startGroup('GCP info');
+    if(core.getInput('test')) {
+      core.startGroup('GAE project info');
       execSync(`gcloud info`, {stdio: 'inherit'});
       core.endGroup();
     }
     else {
-      core.startGroup('Depoy GCP project');
+      core.startGroup('Depoy GAE project');
       execSync(`gcloud app deploy`, {stdio: 'inherit'});
       core.endGroup();
     }
-    core.startGroup('Working with SERVICE_ACCOUNT');
+    core.startGroup(' SERVICE_ACCOUNT');
     console.log('Remove SERVICE_ACCOUNT');
     fs.unlinkSync(serviceAccountFile);
     core.endGroup();
@@ -50,4 +38,4 @@ async function run() {
   }
 }
 
-run()
+run();
